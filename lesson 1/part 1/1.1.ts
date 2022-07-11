959e38399677d3a243fd91e1af5d5348e2014b8f
gerimsol <solflos96@gmail.com>
2022-07-11 21:10:29 +0600

    initial commit

---
+++ lesson 1/part 1/1.1.ts
@@ -0,0 +1,43 @@
+// нелюбимый актер леонардо ди каприо
+
+const firstName: string = 'Leonardo';
+const lastName: string = 'DiCaprio';
+const age: number = 47;
+const fromkz: boolean = false;
+/*
+конец
+*/
+// adele
+
+const realFirstName: string = 'Adele';
+const realLastName: string = 'Adkins';
+const fullYears: number = 34;
+const bornInUk: boolean = true; 
+//
+
+//sophie
+
+const nameOfCharacter: string = 'Sophie';
+const hairColor: string = 'silver';
+const yearsOld: number = 18;
+const cursed: boolean = true; //
+
+//michael scott
+const nameInTheShow: string = 'Michael';
+const lastNameInTheShow: string = 'Scott';
+const wearsSuits: boolean = true;
+const gender: string ='male';
+//
+
+//irimshik
+const cusine: string = 'kazakh';
+const taste: string = 'sweet';
+const sructure: string = 'rubber';
+const milkProduct: boolean = true;
+//
+
+//утка
+const inEnglish: string = 'duck';
+const family: string = 'duck';
+const canSwim: boolean = false;
+const averageAge: number = 7;
